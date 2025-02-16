8a51a1c971eb5958aeb987a7727cb6ee13a81e18
Vriddhi23 <98510238+Vriddhi23@users.noreply.github.com>
2025-02-16 21:42:11 +0530

    Docker Files added

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Wait for database to be ready
+echo "Waiting for MySQL to be ready..."
+while ! mysqladmin ping -h "db" --silent; do
+    sleep 1
+done
+
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Start server
+echo "Starting server..."
+python manage.py runserver 0.0.0.0:8000 
